c7da87d965c3a07c3baab539f5f11ef5c904a83a
Matthew Higgins <developer@matthewhiggins.com>
2009-09-06 15:53:47 -0700

    change main index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect ':action', :controller => 'static'
   
+  map.root :controller => 'static', :action => 'index'
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
