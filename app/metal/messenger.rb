71eb665902768e0f21da48f1d4390d85cc01d94c
Matthew Higgins <developer@matthewhiggins.com>
2009-09-10 22:23:48 -0700

    Let's log this shit

--- app/metal/messenger.rb
+++ app/metal/messenger.rb
@@ -2,6 +2,7 @@ class Messenger
   NOT_FOUND = [404, {"Content-Type" => "text/html"}, "Not Found"]
   class << self
     def call(env)
+      Rails.logger.info "path info = #{env["PATH_INFO"]}"
       if env["PATH_INFO"] =~ /^\/message\/?(\d*)/
         case env["REQUEST_METHOD"]
           when "GET" then get_messages($1)
@@ -14,11 +15,13 @@ class Messenger
     
     private
       def get_messages(revision)
+        Rails.logger.info "Retrieving message #{revision}"
         [200, {}, Message.since(revision).to_json]
       end
     
       def deliver_message(text)
         new_counter = Message.deliver(text)
+        Rails.logger.info "Saved message #{new_counter}"
         [200, {}, new_counter.to_s]
       end
   end
