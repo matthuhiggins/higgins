459b01e4642ec1be51067ab882e1a41979e1d9ac
Matthew <developer@matthewhiggins.com>
2010-05-07 08:59:38 -0700

    Wee

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,10 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.frameworks = [:action_view, :action_controller]
-  # config.cache_store = :mem_cache_store
+  config.gem 'memcached'
+  config.after_initialize do
+    require 'memcached'
+    config.cache_store = :mem_cache_store, Memcached::Rails.new
+  end
   config.time_zone = 'UTC'
 end
