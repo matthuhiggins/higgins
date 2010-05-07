b85a4f1f0353137524859f64cbc5d90770a1bac7
Matthew <developer@matthewhiggins.com>
2010-05-07 08:50:37 -0700

    Chat server away!

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # config.frameworks -= [:active_record, :active_resource, :action_mailer]
   config.frameworks = [:action_view, :action_controller]
   # config.cache_store = :mem_cache_store
   config.time_zone = 'UTC'
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':action', :controller => 'static'
   
   map.root :controller => 'static', :action => 'index'
-  
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
