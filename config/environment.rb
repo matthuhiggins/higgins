375638b4bd9cb1b1e18f08a196e645203c51ed6f
Matthew <developer@matthewhiggins.com>
2010-05-27 14:41:03 -0700

    Remove shit

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,7 @@
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.frameworks = [:action_view, :action_controller]
-  config.gem 'memcached'
-  config.after_initialize do
-    require 'memcached'
-    config.cache_store = :mem_cache_store, Memcached::Rails.new
-  end
   config.time_zone = 'UTC'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :message
-  map.connect 'chat', :controller => 'chat'
   map.connect ':action', :controller => 'static'
-  
   map.root :controller => 'static', :action => 'index'
 end
