d72abb6c0fe078fafec199d36e0122c6c1d3c1ef
Matthew Higgins <developer@matthewhiggins.com>
2010-11-08 09:20:31 -0800

    new style

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0'
+gem 'fb_rails', '0.3.0'
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Higgins
   class Application < Rails::Application
     config.active_support.deprecation = :stderr
     config.secret_token = 'c08895573baf2c545629d3394bd1ed97'
+    config.facebook = {
+      app_id: '140646375985277',
+      secret: '16abc1c5a4a7846a571f121b5d6c977d'
+    }
   end
 end
