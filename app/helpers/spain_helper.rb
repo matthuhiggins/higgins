80b20805641765406f764fedce5ded1b5b445f69
Matthew Higgins <developer@matthewhiggins.com>
2012-10-15 23:08:23 -0700

    thumbs

--- app/helpers/spain_helper.rb
+++ app/helpers/spain_helper.rb
@@ -5,13 +5,20 @@ module SpainHelper
 
   def spain_image(folder, src)
     link_to image_path("spain/#{folder}/#{src}"), rel: "lightbox[#{folder}]" do
-      image_tag "spain/#{folder}/#{src}", height: '60'
+      image_tag "spain/thumbs/#{folder}/#{src}", height: '60'
       # image_tag "spain/thumbnail/#{folder}/#{src}", height: '60'
     end
   end
 end
 
-# Dir[Rails.root.join('app/assets/images/spain/**/*.jpg').to_s].map do |src|
-#   p File.basename(src)
-#   `convert --resize 25% thumbnail`
-# end
+# root_path = Rails.root.join('app/assets/images/spain').to_s
+# Dir["#{root_path}/**/*.jpg"].map do |orig|
+#   next if orig =~ /thumbs/
+#   
+#   thumb_path = orig.gsub(root_path, "#{root_path}/thumbs")
+#   FileUtils.mkdir_p(File.dirname(thumb_path))
+#   # `convert --scale 25% #{orig} #{thumb_path}`
+#   cmd = "convert #{orig} -thumbnail 100x100^ -gravity center -extent 60x60 #{thumb_path}"
+#   p cmd
+#   `#{cmd}`
+# end.size
